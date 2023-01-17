42266f1ff087a1e3816617a9ddd5c0da327441a1
Basopp <75244064+Basopp@users.noreply.github.com>
2023-01-17 21:50:35 +0100

    Update USB_echo.rs

--- examples/USB_echo.rs
+++ examples/USB_echo.rs
@@ -3,72 +3,62 @@
 
 use panic_semihosting as _;
 use stm32g473_hal_oppe as hal;
+
+use hal::usb::Peripheral;
+use hal::{
+    rcc::{CK48Src, Config, RccExt, SysClockSrc},
+    stm32,
+};
+pub use stm32_usbd::UsbBus;
+
 use cortex_m_rt::entry;
 
-use hal::{rcc::{Config, RccExt,SysClockSrc,CK48Src},stm32,prelude::* };
-use hal::usb::{Peripheral};
-pub use stm32_usbd::UsbBus; 
-
+//USB drivers
 use usb_device::prelude::*;
 use usbd_serial::{SerialPort, USB_CLASS_CDC};
 
-
 #[entry]
 fn main() -> ! {
-   
     let dp = stm32::Peripherals::take().unwrap();
-    let mut rcc = dp.RCC.constrain();
-    // Hier stellen we de klok op een eigen instelling in, we gebruiken de standaard interne klok (16Mhz), maar ook de USB klok (48Mhz)
-    let mut rcc = rcc.freeze(Config::new2(SysClockSrc::HSI, CK48Src::HSI48));
-    let mut count = 0;
     
-    let usb = Peripheral {
-        usb: dp.USB,
-        
-    };
+    // Hier stellen we de klok op een eigen instelling in, we gebruiken de standaard interne klok (16Mhz), maar ook de USB klok (48Mhz)
+    dp.RCC.freeze(Config::new2(SysClockSrc::HSI, CK48Src::HSI48));
+    
+
+    //USB peripheral gebruiken
+    let usb = Peripheral { usb: dp.USB };
     let usb_bus = UsbBus::new(usb);
 
+    //seriele poort aanmaken
     let mut serial = SerialPort::new(&usb_bus);
 
+    //USB instellingen en de device aanmaken
     let mut usb_dev = UsbDeviceBuilder::new(&usb_bus, UsbVidPid(0x16c0, 0x27dd))
-        .manufacturer("Aemics")
         .device_class(USB_CLASS_CDC)
         .build();
 
     loop {
-        
-        if !usb_dev.poll(&mut [&mut serial]) {
-            continue;
-        }
 
+        usb_dev.poll(&mut [&mut serial]);
+
+        //buffer aanmaken voor de karakters die we gaan typen
         let mut buf = [0u8; 64];
 
+        //inkomende data lezen
         match serial.read(&mut buf) {
-            Ok(count) if count > 0 => {
-               
-
+            Ok(_) => {
                 // bit veranderen zodat het in hoofdletters terug echo't
-                for c in buf[0..count].iter_mut() {
+                for c in buf[0..64].iter_mut() {
                     if 0x61 <= *c && *c <= 0x7a {
                         *c &= !0x20;
                     }
                 }
-
-                let mut write_offset = 0;
-                while write_offset < count {
-                    match serial.write(&buf[write_offset..count]) {
-                        Ok(len) if len > 0 => {
-                            write_offset += len;
-                        }
-                        _ => {}
-                    }
-                }
+            
+            //aangepaste data terugsturen
+            serial.write(&buf).unwrap();
+             
             }
-            _ => {}
+        _ => {}
         }
-
-        
     }
-
 }
-
