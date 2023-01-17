e8a7e417c244e1de23390e6e6d94da84e5d97256
Basopp <75244064+Basopp@users.noreply.github.com>
2023-01-17 21:49:13 +0100

    Update LED_interrupt.rs

--- examples/LED_interrupt.rs
+++ examples/LED_interrupt.rs
@@ -4,62 +4,64 @@
 use panic_semihosting as _;
 use stm32g473_hal_oppe as hal;
 
-use hal::{  stm32::{interrupt,Interrupt,self}, 
-            prelude::*, 
-            
-            gpio::GpioExt, 
-            rcc::{Config, RccExt},
-            timer::{Timer,Event, CountDownTimer},
-        };
+//Hier importeren we de benodigde functies uit de HAL die we nodig hebben
+use hal::{
+    gpio::GpioExt,
+    prelude::*,
+    rcc::{Config, RccExt},
+    stm32::{self, interrupt, Interrupt},
+    timer::{CountDownTimer, Event, Timer},
+};
 
 use core::cell::RefCell;
 use core::ops::DerefMut;
 use cortex_m::{asm::wfi, interrupt::Mutex};
 use cortex_m_rt::entry;
 
-static TIMER_TIM2: Mutex<RefCell<Option<CountDownTimer<stm32::TIM2>>>> = Mutex::new(RefCell::new(None));
+//We gebruiken hier een Mutex voor de Timer. dit zorgt er voor dat maar 1 proces tegelijkertijd de waarde kan veranderen.
+static TIMER_TIM2: Mutex<RefCell<Option<CountDownTimer<stm32::TIM2>>>> =
+    Mutex::new(RefCell::new(None));
 
+//Hier komt de code voor de interrupt. Eerst zetten we de interrupt bit weer uit, daarna herstart de timer.
 #[interrupt]
 fn TIM2() {
-   
- 
+    //Laadt de timer in de interrupt
     cortex_m::interrupt::free(|cs| {
-            if let Some(ref mut t2) = TIMER_TIM2.borrow(cs).borrow_mut().deref_mut() {
-                t2.clear_interrupt(Event::TimeOut);
-                t2.start(100.ms())
-            }
-        });
- 
+        if let Some(ref mut t2) = TIMER_TIM2.borrow(cs).borrow_mut().deref_mut() {
+            //Hier resetten we de interrupt
+            t2.clear_interrupt(Event::TimeOut);
+            
+        }
+    });
 }
-
 
 #[entry]
 fn main() -> ! {
-  
-
-    
+    //Peripherals in laden
     let dp = stm32::Peripherals::take().unwrap();
     let mut rcc = dp.RCC.freeze(Config::hsi());
 
+    //Pin goed zetten
     let gpiob = dp.GPIOB.split(&mut rcc);
     let mut led = gpiob.pb7.into_push_pull_output();
-    
+
+    //Timer initialiseren
     let timer2 = Timer::new(dp.TIM2, &rcc.clocks);
-    
+
+    //Timer starten
     let mut timer2_i = timer2.start_count_down(100.ms());
     timer2_i.listen(Event::TimeOut);
 
     cortex_m::interrupt::free(|cs| TIMER_TIM2.borrow(cs).borrow_mut().replace(timer2_i));
 
-
+    //interrupt mogelijk maken
     unsafe {
         cortex_m::peripheral::NVIC::unmask(Interrupt::TIM2);
-        
     }
-    
+
     loop {
+        //We hoeven alleen te wachten op een interrupt, en dan de led te veranderen
         wfi();
         led.toggle().unwrap();
-      
     }
 }
