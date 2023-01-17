78247bc4b4ec98be6e3716cee2d70babbff7f0fa
Basopp <75244064+Basopp@users.noreply.github.com>
2023-01-17 21:51:16 +0100

    Update USB_print.rs

--- examples/USB_print.rs
+++ examples/USB_print.rs
@@ -3,64 +3,54 @@
 
 use panic_semihosting as _;
 use stm32g473_hal_oppe as hal;
+
+use hal::usb::Peripheral;
+use hal::{
+    rcc::{CK48Src, Config, RccExt, SysClockSrc},
+    stm32,
+};
+pub use stm32_usbd::UsbBus;
+
 use cortex_m_rt::entry;
 
-use hal::{rcc::{Config, RccExt,SysClockSrc,CK48Src},stm32,prelude::* };
-use hal::usb::{Peripheral};
-pub use stm32_usbd::UsbBus; 
-
+//USB drivers
 use usb_device::prelude::*;
 use usbd_serial::{SerialPort, USB_CLASS_CDC};
 
-
 #[entry]
 fn main() -> ! {
-   
     let dp = stm32::Peripherals::take().unwrap();
-    let mut rcc = dp.RCC.constrain();
-    // Hier stellen we de klok op een eigen instelling in, we gebruiken de standaard interne klok (16Mhz), maar ook de USB klok (48Mhz)
-    let mut rcc = rcc.freeze(Config::new2(SysClockSrc::HSI, CK48Src::HSI48));
-    let mut count = 0;
     
-    let usb = Peripheral {
-        usb: dp.USB,
-        
-    };
+    // Hier stellen we de klok op een eigen instelling in, we gebruiken de standaard interne klok (16Mhz), maar ook de USB klok (48Mhz)
+    dp.RCC.freeze(Config::new2(SysClockSrc::HSI, CK48Src::HSI48));
+    let mut count = 0;
+
+    //USB peripheral gebruiken
+    let usb = Peripheral { usb: dp.USB };
     let usb_bus = UsbBus::new(usb);
 
+    //seriele poort aanmaken
     let mut serial = SerialPort::new(&usb_bus);
 
+    //USB instellingen en de device aanmaken
     let mut usb_dev = UsbDeviceBuilder::new(&usb_bus, UsbVidPid(0x16c0, 0x27dd))
-        .manufacturer("Fake company")
-        .product("Serial port")
-        .serial_number("TEST")
         .device_class(USB_CLASS_CDC)
         .build();
 
     loop {
-        
-    usb_dev.poll(&mut [&mut serial]);
-    
-    count +=1; 
-    // om wat tijd tussen onze berichten te houden zetten we een counter in, die maar 1 keer in de 200000 clock cycles iets print
-    if count == 200000 {
+         //De host moet weten of de device iets wil verzenden. Dit moet (minimaal) om de milliseconde aangeroepen worden
+         usb_dev.poll(&mut [&mut serial]);
 
-    // Hier zetten we het bericht dat we wilen printen. Deze wordt gelijk omgezet naar bytes en de lengte wordt berekent
-    let mut x = b"Test bericht \n" ;
-    let mut size = x.len();
-   
-    let mut write_offset = 0;
-  
-    match serial.write(&x[write_offset..size]) {
-                        Ok(len) if len > 0 => {
-                            write_offset += len;
-                        }
-                        _ => {}
-                 } 
-        }
-        else if count == 200001{
-            count = 0;
-        }
-        
+         count += 1;
+         // om wat tijd tussen onze berichten te houden zetten we een counter in, die maar 1 keer in de 200000 clock cycles iets print
+         if count == 200000 {
+             // Hier zetten we het bericht dat we willen printen. Deze wordt gelijk omgezet naar bytes en dan geprint
+             let data = b"Test bericht \n";
+             serial.write(data).unwrap();
+ 
+         //counter resetten
+         } else if count == 200001 {
+             count = 0;
+         }
     }
 }
