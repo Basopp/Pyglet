9cb1a4f3b9a1c4f42407a0d82164832e9a11b8b6
Basopp <75244064+Basopp@users.noreply.github.com>
2023-01-18 10:42:46 +0100

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -2,70 +2,12 @@
 #![no_std]
 
 use panic_semihosting as _;
-
-use stm32g473_hal_oppe as hal;
-use ws2812_timer_delay as ws2812;
-
-use crate::hal::delay::Delay;
-use crate::hal::prelude::*;
-use crate::hal::stm32;
-use crate::hal::time::Hertz;
-use crate::hal::timer::*;
-use crate::ws2812::Ws2812;
-use cortex_m::peripheral::Peripherals;
-
-use smart_leds::{SmartLedsWrite, RGB8};
-
 use cortex_m_rt::entry;
 
 #[entry]
 fn main() -> ! {
-    
 
-    let dp = stm32::Peripherals::take().unwrap();
-    let cp = cortex_m::Peripherals::take().unwrap();
-    let mut rcc = dp.RCC.constrain();
-        // Constrain clocking registers
-        
-        let gpiob = dp.GPIOB.split(&mut rcc);
-        let gpioa = dp.GPIOA.split(&mut rcc);
+   loop {
 
-        /* (Re-)configure PA7 as output */
-        let ws_data_pin = gpiob.pb10.into_push_pull_output();
-
-        let timer = Timer::tim1(p.TIM1, 3.mhz, &mut rcc);
-
-        // Get delay provider
-        let mut delay = Delay::new(cp.SYST, &mut rcc);
-
-        let mut ws = Ws2812::new(timer, ws_data_pin);
-        let mut data: [RGB8; 3] = [RGB8::default(); 3];
-        let empty: [RGB8; 3] = [RGB8::default(); 3];
-
-        data[0] = RGB8 {
-            r: 0,
-            g: 0,
-            b: 0x10,
-        };
-        data[1] = RGB8 {
-            r: 0,
-            g: 0x10,
-            b: 0,
-        };
-        data[2] = RGB8 {
-            r: 0x10,
-            g: 0,
-            b: 0,
-        };
-
-        loop {
-            ws.write(data.iter().cloned()).unwrap();
-            delay.delay_ms(1000 as u16);
-            ws.write(empty.iter().cloned()).unwrap();
-            delay.delay_ms(1000 as u16);
         }
-    }
-    loop {
-        continue;
-    }
 }
